cc6cc540cb70b4909e4e6b17376a060d86fa193d
kaizer <110983412+kkkaizer@users.noreply.github.com>
2023-11-07 11:24:49 -0300

    Add files via upload

---
+++ coolsongs-html/laura 2b/aluramidi-curso-aula1/main.js
@@ -0,0 +1,13 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play ();
+}
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+contador = 0
+
+while (contador < ListaDeTeclas.length) {
+    ListaDeTeclas[contador].onclick = tocaSomPom;
+    contador = contador + 1;
+    console.log(contador);
+}
